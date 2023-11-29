179c446e2035d53aa998d8a278e08ffebca56d64
MichaelDecent <michaeldecent2@gmail.com>
2023-11-29 12:00:41 +0000

    fix task 101

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
   let nameList = '';
   for (const [index, name] of Object.entries(reportWithIterator)) {
-    if (parseInt(index) === (reportWithIterator.length - 1)) {
+    if (parseInt(index, 10) === (reportWithIterator.length - 1)) {
       nameList += name;
     } else {
       nameList += `${name} | `;
