f486c7d3c61bd9c54a45be2a347a51eeb3bd5226
MichaelDecent <michaeldecent2@gmail.com>
2023-11-30 01:59:27 +0000

    fix errors

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,19 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const user = signUpUser(firstName, lastName);
-  const photo = uploadPhoto(fileName);
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([user, photo])
-    .then((result) => {
-      const output = [];
-      result.forEach((element) => {
-        if (element.status === 'fulfilled') {
-          result.push({ status: element.status, value: element.value });
-        } else {
-          result.push({ status: element.status, value: `${element.reason}` });
-        }
-      });
-      return output;
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
     });
+    return result;
+  });
 }
