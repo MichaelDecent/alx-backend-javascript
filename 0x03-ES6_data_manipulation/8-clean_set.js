5140e68269d7693385d1e5b69acf932cd69e3ee0
MichaelDecent <michaeldecent2@gmail.com>
2023-12-05 22:31:10 +0000

    fixed error

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,10 @@
 export default function cleanSet(setArray, checkString) {
   if (checkString) {
     const startString = Array.from(setArray).filter((value) => value.startsWith(checkString));
-    const cleanedStrings = startString.map((value) => value.substring(checkString.length));
-
-    return cleanedStrings.join('-');
+    if (startString) {
+        const cleanedStrings = startString.map((value) => value.substring(checkString.length));
+	return cleanedStrings.join('-');
+    }
   }
   return '';
 }
