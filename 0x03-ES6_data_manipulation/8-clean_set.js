5365a62ea964c868ef96441e20cd620c395e176e
MichaelDecent <michaeldecent2@gmail.com>
2023-12-05 23:09:09 +0000

    added task 9-10 solution

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(setArray, checkString) {
   if (checkString === undefined || checkString.length === 0) {
-   return '';
+    return '';
   }
   const startString = Array.from(setArray).filter((value) => value.startsWith(checkString));
   const cleanedStrings = startString.map((value) => value.substring(checkString.length));
