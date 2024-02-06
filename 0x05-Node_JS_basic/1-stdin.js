3968c0db48105404f65d8302777c18aca41c5c86
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-02-06 15:07:44 +0100

    fixed errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,11 +2,11 @@ process.stdin.setEncoding('utf8');
 
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', function(data) {
-  console.log('Your name is:', data.trim());
-  
-  console.log('This important software is now closing');
+process.stdin.on('data', function (data) {
+    console.log('Your name is:', data.trim());
 
-  process.exit();
+    console.log('This important software is now closing');
+
+    process.exit();
 });
 
