1ab06f4c81a0ea8fa0d951cef4ecb58c5a80f9c2
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-02-07 03:49:26 +0100

    updated task 0-2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,38 +1,41 @@
 const fs = require('fs');
 
-function countStudents(fileName) {
-  const students = {};
-  const fields = {};
-  let length = 0;
-  try {
-    const content = fs.readFileSync(fileName, 'utf-8');
-    const lines = content.toString().split('\n');
-    for (let i = 0; i < lines.length; i += 1) {
-      if (lines[i]) {
-        length += 1;
-        const field = lines[i].toString().split(',');
-        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-          students[field[3]].push(field[0]);
-        } else {
-          students[field[3]] = [field[0]];
-        }
-        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-          fields[field[3]] += 1;
-        } else {
-          fields[field[3]] = 1;
-        }
-      }
+function countStudents(filePath) {
+    try {
+        
+        const lines = fs.readFileSync(filePath, 'utf8').split('\n');
+        const csArray = [];
+        const sweArray = [];
+
+        let count = -1, csCount = 0, sweCount = 0;
+
+        lines.forEach((line, index) => {
+            if (line.length !== 0) {
+                cols = line.split(',');
+                field = cols[cols.length - 1].trim();
+
+                if (field === 'CS') {
+                    csArray.push(cols[0]); 
+                    csCount++
+                } else if (field === 'SWE') {
+                    sweArray.push(cols[0]);
+                    sweCount++
+                }
+
+                count++;
+            }
+        });
+
+        const csStudents = csArray.join(", ")
+        const sweStudents = sweArray.join(", ")
+
+        console.log(`Number of students: ${count}`);
+        console.log(`Number of students in CS: ${csCount}. List: ${csStudents}`)
+        console.log(`Number of students in SWE: ${sweCount}. List: ${sweStudents}`)
+    } catch (error) {
+        throw Error('Cannot load the database');
     }
-    const l = length - 1;
-    console.log(`Number of students: ${l}`);
-    for (const [key, value] of Object.entries(fields)) {
-      if (key !== 'field') {
-        console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
-      }
-    }
-  } catch (error) {
-    throw Error('Cannot load the database');
-  }
+    
 }
 
 module.exports = countStudents;
---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+function countStudents(filePath) {
+    return new Promise((resolve, reject) => {
+
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                reject(new Error('Cannot load the database'));
+                return;
+            }
+
+            const lines = data.split('\n');
+            const csArray = [];
+            const sweArray = [];
+
+            let count = -1, csCount = 0, sweCount = 0;
+
+            lines.forEach((line, index) => {
+                if (line.length !== 0) {
+                    cols = line.split(',');
+                    field = cols[cols.length - 1].trim();
+
+                    if (field === 'CS') {
+                        csArray.push(cols[0]);
+                        csCount++
+                    } else if (field === 'SWE') {
+                        sweArray.push(cols[0]);
+                        sweCount++
+                    }
+
+                    count++;
+                }
+            });
+
+            const csStudents = csArray.join(", ")
+            const sweStudents = sweArray.join(", ")
+
+            console.log(`Number of students: ${count}`);
+            console.log(`Number of students in CS: ${csCount}. List: ${csStudents}`)
+            console.log(`Number of students in SWE: ${sweCount}. List: ${sweStudents}`)
+            resolve(data)
+        });
+    })
+}
+
+module.exports = countStudents;
