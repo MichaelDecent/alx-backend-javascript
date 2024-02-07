6945df34519313f978449e46c8f5493381a627bc
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-02-07 12:55:49 +0100

    added task 6 & 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const countStudents = require('./3-read_file_async');
 
-const app = express;
+const app = express();
 
 const hostname = "127.0.0.1";
 const port = 1245;
